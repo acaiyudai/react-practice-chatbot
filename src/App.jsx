ca6dedee2a48d88bbf43f389d7f4dceb4cd74b60
acaiyudai <ispectrene@outlook.com>
2022-09-10 21:09:10 +0900

    Add : create css and set App.jsx and index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/style.css'
+import React from 'react';
+import { Component } from 'react';
+import defaultDataset from './dataset'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: 'init',
+      dataset: defaultDataset,
+      open: false
+    }
+  }
+
+  render(){
+    return (
+      <section className='c-section'>
+        <div className='c-box'>
+        </div>
+      </section>
+    );
+  }
+  
 }
 
 export default App;
